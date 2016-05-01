b002ad452b06f4c99a0f3c81b0ca1dc701020227
AliceTheGorgon <alicethegorgon@gmail.com>
2016-05-01 13:15:25 -0500

    Add version info. Put plugins into their own directory.

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-RELEASEDIR=../../EvaFuel/
-rm "$RELEASEDIR/*"
+RELEASEDIR=../../EvaFuel
+rm -rf $RELEASEDIR
+mkdir -p "$RELEASEDIR/Plugins/"
 cp KIS_Fuel_Tank.cfg "$RELEASEDIR"
 cp LICENSE "$RELEASEDIR"
 cp CREDITS "$RELEASEDIR"
-cp ./EvaFuel/bin/Release/EvaFuel.dll "$RELEASEDIR"
-cp ./EvaFuel-KISCompat/bin/Release/EvaFuel-KISCompat.dll "$RELEASEDIR"
+cp EvaFuel.version "$RELEASEDIR"
+cp ./EvaFuel/bin/Release/EvaFuel.dll "$RELEASEDIR/Plugins/"
+cp ./EvaFuel-KISCompat/bin/Release/EvaFuel-KISCompat.dll "$RELEASEDIR/Plugins/"
