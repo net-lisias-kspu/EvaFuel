dfe117b49e25e22b9a778ff5e7499aaa44b1d9d8
AliceTheGorgon <alicethegorgon@gmail.com>
2016-05-02 12:25:38 -0500

    Rescue contract ships will now give the Kerbal 1 unit of EvaPropellant to use.

--- EvaFuel/EvaFuelManager.cs
+++ EvaFuel/EvaFuelManager.cs
@@ -19,12 +19,32 @@ namespace EvaFuel {
 
 		public void onEvaStart(GameEvents.FromToAction<Part, Part> data) {
 			double ShipMonoProp = data.from.RequestResource("MonoPropellant", EVATankFuelMax);
-			data.to.RequestResource("EVA Propellant", EVATankFuelMax - ShipMonoProp);
+
+			//debug
+			print("EvaFuel, part count: " + data.from.vessel.Parts.Count.ToString());
+
 			if (ShipMonoProp == EVATankFuelMax) {
+				data.to.RequestResource("EVA Propellant", EVATankFuelMax - ShipMonoProp);
 				ScreenMessages.PostScreenMessage ("Filled EVA tank with " + Math.Round(ShipMonoProp, 2).ToString() + " units of MonoPropellant.", ScreenMessageLife, ScreenMessageStyle.UPPER_CENTER);
 			}
 			if (ShipMonoProp < EVATankFuelMax) {
-				ScreenMessages.PostScreenMessage ("Warning! Only " + Math.Round(ShipMonoProp, 2).ToString() + " units of MonoPropellant for EVA!", ScreenMessageWarningLife, ScreenMessageStyle.UPPER_CENTER);
+				if (ShipMonoProp == 0) { //Check if it's likely a rescue contract ship.
+					double ShipElectricity = data.from.RequestResource ("ElectricCharge", 1);
+					if (ShipElectricity == 0) {
+						if (data.from.vessel.Parts.Count == 1) { //only one part on ship
+							data.to.RequestResource ("EVA Propellant", EVATankFuelMax - 1);//give one unit of eva propellant
+							ScreenMessages.PostScreenMessage ("The Kerbal manages to scrounge together 1 unit of EVA Propellant.", ScreenMessageWarningLife, ScreenMessageStyle.UPPER_CENTER);
+						}
+					} else { //This has electricity, and thus isn't a rescue contract ship.
+						data.from.RequestResource ("ElectricCharge", -1);
+						data.to.RequestResource ("EVA Propellant", EVATankFuelMax - ShipMonoProp);
+						ScreenMessages.PostScreenMessage ("Warning! No MonoPropellant for EVA!", ScreenMessageWarningLife, ScreenMessageStyle.UPPER_CENTER);
+					}
+
+				} else {
+					data.to.RequestResource("EVA Propellant", EVATankFuelMax - ShipMonoProp);
+					ScreenMessages.PostScreenMessage ("Warning! Only " + Math.Round(ShipMonoProp, 2).ToString() + " units of MonoPropellant for EVA!", ScreenMessageWarningLife, ScreenMessageStyle.UPPER_CENTER);
+				}
 			}
 		}
 
