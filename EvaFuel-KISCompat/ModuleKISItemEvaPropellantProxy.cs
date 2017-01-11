ab6996a25ae4aa91a67ac2a1285841e525c56055
Shinsaka <shinji.sakanami@gmail.com>
2017-01-11 17:51:54 +0100

    Add configuration file
    Add a configuration file used to change resources name and quantity.

--- EvaFuel-KISCompat/ModuleKISItemEvaPropellantProxy.cs
+++ EvaFuel-KISCompat/ModuleKISItemEvaPropellantProxy.cs
@@ -1,32 +1,61 @@
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-using System.Linq;
-using System.Text;
-using KSPDev.GUIUtils;
 using KIS;
+using KSPDev.GUIUtils;
+using System;
+
 [assembly: KSPAssemblyDependency("KIS", 1, 1)]
+namespace EvaFuel
+{
+    public class ModuleKISItemEvaPropellantProxy : ModuleKISItemEvaPropellant
+    {
+        public EvaFuelSettings settings { get; set; }
 
-namespace EvaFuel {
-	public class ModuleKISItemEvaPropellantProxy : ModuleKISItemEvaPropellant {
+        public override void OnAwake()
+        {
+            this.settings = new EvaFuelSettings();
+            this.LoadSettings();
+            base.OnAwake();
+        }
 
-		public override void OnItemUse(KIS_Item item, KIS_Item.UseFrom useFrom) {
-			if (useFrom != KIS_Item.UseFrom.KeyUp && item.inventory.invType == ModuleKISInventory.InventoryType.Pod) {
-				double FuelLeft = 0;
-				double FuelMax = 0;
-				FuelLeft = GetCanisterFuelResource (item).amount;
-				FuelMax = GetCanisterFuelResource (item).maxAmount;
+        public void LoadSettings()
+        {
+            ConfigNode[] nodes;
+            nodes = GameDatabase.Instance.GetConfigNodes("EvaFuelSettings");
+            if (nodes != null)
+            {
+                for (int i = 0; i < nodes.Length; i++)
+                {
+                    this.settings.Load(nodes[i]);
+                }
+            }
+        }
 
-				double ShipMonoProp = item.inventory.part.RequestResource ("MonoPropellant", FuelMax - FuelLeft);
-				item.SetResource ("EVA Propellant", FuelLeft + ShipMonoProp);
+        public override void OnItemUse(KIS_Item item, KIS_Item.UseFrom useFrom)
+        {
+            string shipProp = this.settings.ShipPropellantName;
+            string evaProp = this.settings.EvaPropellantName;
 
-				if (ShipMonoProp < FuelMax - FuelLeft) {
-					ScreenMessaging.ShowPriorityScreenMessage("Warning! Only " + Math.Round(ShipMonoProp, 2).ToString() + " units of MonoPropellant were left to fill the tank!");
-				} else {
-					ScreenMessaging.ShowPriorityScreenMessage("Fuel tank refueled with " + Math.Round(ShipMonoProp, 2).ToString() + " units of MonoPropellant.");
+            if (useFrom != KIS_Item.UseFrom.KeyUp && item.inventory.invType == ModuleKISInventory.InventoryType.Pod)
+            {
+				double fuelLeft = 0;
+				double fuelMax = 0;
+				fuelLeft = GetCanisterFuelResource(item).amount;
+				fuelMax = GetCanisterFuelResource(item).maxAmount;
+
+				double fuelRequest = item.inventory.part.RequestResource(shipProp, fuelMax - fuelLeft);
+				item.SetResource(evaProp, fuelLeft + fuelRequest);
+
+				if (fuelRequest < fuelMax - fuelLeft)
+                {
+					ScreenMessaging.ShowPriorityScreenMessage("Warning! Only " + Math.Round(fuelRequest, 2).ToString() + " units of " + shipProp + " were left to fill the tank!");
+				}
+                else
+                {
+					ScreenMessaging.ShowPriorityScreenMessage("Fuel tank refueled with " + Math.Round(fuelRequest, 2).ToString() + " units of " + shipProp + ".");
 				}
 				UISoundPlayer.instance.Play(refuelSndPath);
-			} else {
+			}
+            else
+            {
 				base.OnItemUse(item, useFrom);
 			}
 		}
---
+++ EvaFuel/EvaFuelSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace EvaFuel
+{
+    public class EvaFuelSettings
+    {
+        public double EvaTankFuelMax;
+        public string EvaPropellantName;
+        public string ShipPropellantName;
+        public string ShipElectricityName;
+
+        public int ScreenMessageLife;
+        public int ScreenMessageWarningLife;
+
+        public EvaFuelSettings()
+        {
+            this.EvaTankFuelMax = 5;
+            this.EvaPropellantName = "EVA Propellant";
+            this.ShipPropellantName = "MonoPropellant";
+            this.ShipElectricityName = "ElectricCharge";
+
+            this.ScreenMessageLife = 5;
+            this.ScreenMessageWarningLife = 10;
+        }
+
+        public void Load(ConfigNode node)
+        {
+            node.TryGetValue("EvaTankFuelMax", ref this.EvaTankFuelMax);
+            node.TryGetValue("EvaPropellantName", ref this.EvaPropellantName);
+            node.TryGetValue("ShipPropellantName", ref this.ShipPropellantName);
+            node.TryGetValue("ShipElectricityName", ref this.ShipElectricityName);
+            node.TryGetValue("ScreenMessageLife", ref this.ScreenMessageLife);
+            node.TryGetValue("ScreenMessageWarningLife", ref this.ScreenMessageWarningLife);
+        }
+    }
+}
