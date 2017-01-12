90e5c2890c68469e919c2ccf2075543f6c5ece77
AliceTheGorgon <alicethegorgon@gmail.com>
2017-01-12 02:03:22 -0600

    Fixing compilation

--- copyfiles.sh
+++ copyfiles.sh
@@ -3,10 +3,12 @@ RELEASEDIR=../Release/EvaFuel
 rm -rf $RELEASEDIR
 mkdir -p "$RELEASEDIR/Plugins/"
 cp KIS_Fuel_Tank.cfg "$RELEASEDIR"
+cp EvaFuelSettings.cfg "$RELEASEDIR"
 cp LICENSE "$RELEASEDIR"
 cp CREDITS "$RELEASEDIR"
 cp EvaFuel.version "$RELEASEDIR"
 cp ./EvaFuel/bin/Release/EvaFuel.dll "$RELEASEDIR/Plugins/"
 cp ./EvaFuel-KISCompat/bin/Release/EvaFuel-KISCompat.dll "$RELEASEDIR/Plugins/"
+cp ../Mod\ Assemblies/KSPDev_Utils.dll "$RELEASEDIR/Plugins/"
 cd ../Release/
 zip -r -9 "EvaFuel-$(date --universal +%F-%T).zip" "EvaFuel"
