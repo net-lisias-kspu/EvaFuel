245703c2ad985626ca1b23d2708f61f47bdcc588
AliceTheGorgon <alicethegorgon@gmail.com>
2017-01-20 05:02:03 -0600

    Rescue contract check now parses contract list for Kerbal name, instead of checking for an empty vessel. Some vessels where spawning non-empty but were rescue contract ships.

--- EvaFuel/EvaFuelManager.cs
+++ EvaFuel/EvaFuelManager.cs
@@ -32,7 +32,6 @@ namespace EvaFuel
         public void onEvaStart(GameEvents.FromToAction<Part, Part> data)
         {
             string shipProp = this.settings.ShipPropellantName;
-            string shipElec = this.settings.ShipElectricityName;
             string evaProp = this.settings.EvaPropellantName;
             double evaFuelMax = this.settings.EvaTankFuelMax;
 			double conversionFactor = this.settings.FuelConversionFactor; //conversionFactor for changing ration of ship fuel to Eva Fuel. Take special care to check multiplication or division
@@ -42,33 +41,33 @@ namespace EvaFuel
 			double takenFuel = data.from.RequestResource(shipProp, evaFuelMax / conversionFactor);
 			double fuelRequest = takenFuel * conversionFactor;
 
+			bool rescueShip = false;
+			if (fuelRequest == 0) { //Only check for rescue vessel status if there's no EVA fuel in the current ship.
+				var crewList = data.to.vessel.GetVesselCrew ();
+				if (HighLogic.CurrentGame.Mode == Game.Modes.CAREER) {
+					if (Contracts.ContractSystem.Instance != null) {
+						var contracts = Contracts.ContractSystem.Instance.Contracts;
+						for (int currentCrew = 0; currentCrew < crewList.Count; currentCrew++) {
+							for (int currentContract = 0; currentContract < contracts.Count; currentContract++) {
+								if (contracts [currentContract].Title.Contains (crewList [currentCrew].name) & data.from.vessel.name.Contains (crewList [currentCrew].name.Split (null) [0])) {//Please do not rename your ship to have a rescue contract Kerbal name in it if the contract is active.
+									rescueShip = true;
+								}
+							}
+						}
+					}
+				}
+			}
+
 			if (fuelRequest + 0.001 > evaFuelMax) //Floats and doubles don't like exact numbers. :/ Need to test for similarity rather than equality.
             {
 				data.to.RequestResource(evaProp, evaFuelMax - fuelRequest);
 				ScreenMessages.PostScreenMessage("Filled EVA tank with " + Math.Round(takenFuel, 2).ToString() + " units of " + shipProp + ".", messageLife, ScreenMessageStyle.UPPER_CENTER);
-			} else
-			{
-				if (fuelRequest == 0) //Check if it's likely a rescue contract ship.
-                {
-					double shipElectricity = data.from.RequestResource(shipElec, 1);
-					if (shipElectricity == 0)
-                    {
-						if (data.from.vessel.Parts.Count == 1) //only one part on ship
-                        {
-							data.to.RequestResource(evaProp, evaFuelMax - 1);//give one unit of eva propellant
-							PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "No fuel!", "Warning! There was no fuel aboard ship, so only one single unit of " + evaProp + " was able to be scrounged up for the journey!", "OK", false, HighLogic.UISkin);
-						}
-					} else //This has electricity, and thus isn't a rescue contract ship. Except when it is. :/ TODO: Find out why that one rescue ship had electricity, and find out how to work around that.
-                    {
-						data.from.RequestResource(shipElec, -1);
-						data.to.RequestResource(evaProp, evaFuelMax - fuelRequest);
-						PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Low EVA Fuel!", "Warning! Only " + Math.Round(takenFuel, 2).ToString() + " units of " + shipProp + " were available for EVA! Meaning you only have " + Math.Round(fuelRequest, 2).ToString() + " units of " + evaProp + "!", "OK", false, HighLogic.UISkin);
-					}
-				} else
-                {
-					data.to.RequestResource(evaProp, evaFuelMax - fuelRequest);
-					PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Low EVA Fuel!", "Warning! Only " + Math.Round(takenFuel, 2).ToString() + " units of " + shipProp + " were available for EVA! Meaning you only have " + Math.Round(fuelRequest, 2).ToString() + " units of " + evaProp + "!", "OK", false, HighLogic.UISkin);
-				}
+			} else if (rescueShip == true & fuelRequest == 0) {
+					data.to.RequestResource (evaProp, evaFuelMax - 1);//give one unit of eva propellant
+					PopupDialog.SpawnPopupDialog (new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Rescue fuel!", "Warning! There was no fuel aboard ship, so only one single unit of " + evaProp + " was able to be scrounged up for the journey!", "OK", false, HighLogic.UISkin);
+			} else {
+				data.to.RequestResource (evaProp, evaFuelMax - fuelRequest);
+				PopupDialog.SpawnPopupDialog (new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Low EVA Fuel!", "Warning! Only " + Math.Round(takenFuel, 2).ToString() + " units of " + shipProp + " were available for EVA! Meaning you only have " + Math.Round(fuelRequest, 2).ToString() + " units of " + evaProp + "!", "OK", false, HighLogic.UISkin);
 			}
 		}
 
