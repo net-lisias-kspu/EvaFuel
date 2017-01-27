a79208cb50bd15f38373bbd901c9f75b8971299b
AliceTheGorgon <alicethegorgon@gmail.com>
2017-01-27 03:26:00 -0600

    Split the file copy and release making scripts.

--- copyfiles.sh
+++ copyfiles.sh
@@ -10,5 +10,3 @@ cp EvaFuel.version "$RELEASEDIR"
 cp ./EvaFuel/bin/Release/EvaFuel.dll "$RELEASEDIR/Plugins/"
 cp ./EvaFuel-KISCompat/bin/Release/EvaFuel-KISCompat.dll "$RELEASEDIR/Plugins/"
 cp ../Mod\ Assemblies/KSPDev_Utils.dll "$RELEASEDIR/Plugins/"
-cd ../Release/
-zip -r -9 "EvaFuel-$(date --universal +%F-%T).zip" "EvaFuel"
