9c92efdd9d1722c0cc6246b0b906a6671b31e393
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-11-01 06:48:03 -0400

    Updated path in deploy.bat

--- EvaFuel-KISCompat/InstallChecker.cs
+++ EvaFuel-KISCompat/InstallChecker.cs
@@ -62,10 +62,6 @@ namespace EvaFuel
                     false,
                     HighLogic.UISkin
                 );
-                Debug.Log("Incorrect " + MODNAME + " Installation: " + MODNAME + " has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/" + EXPECTEDPATH + ". Do not move any files from inside that folder.\n\nIncorrect path(s):\n" + String.Join("\n", badPaths.ToArray())
-
-                     );
-
             }
 
             //// Check for Module Manager
--- EvaFuel/AssemblyVersion.cs
+++ EvaFuel/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.6.7.0")]
+ [assembly: AssemblyVersion("1.6.7.2")]
 
